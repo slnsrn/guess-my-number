7dc07f58332dc443429f814b69a6ab74e7666c09
Selin Serin <selin.serin@frontiercargroup.com>
2020-05-16 17:19:33 +0200

    adds fonts

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ export default () => (
         <title>Guess my number</title>
         <meta property="og:title" content="Guess my number" key="title" />
         <meta property="og:description" content="A Cool's game" key="description" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Press+Start+2P"></link>
     </Head>
     <div className='w-full px-4 py-8 md:p-12 xl:px-40 lg:py-12'>
       <h1 className='text-center mb-6 text-white text-2xl md:text-4xl'>Guess my number</h1>
