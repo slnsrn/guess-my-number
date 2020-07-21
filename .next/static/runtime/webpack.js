0011a2986f9ad9709203a3901912e6bcb542d7b9
Selin Serin <selin.serin@frontiercargroup.com>
2020-07-21 18:43:30 +0200

    fix dependecies

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cbec774641f7b34da84a";
+/******/ 	var hotCurrentHash = "41685643f56d1981b28f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
